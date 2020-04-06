44277b21760c242ad05d358b20e083d8188876e1
Minha Kwon <minha61@naver.com>
2020-04-07 04:01:40 +0900

    Update BOJ#1874
    Not solved yet

--- BOJ#1874/src/Main.java
+++ BOJ#1874/src/Main.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
+import java.util.Stack;
 
+/**
+ * @author gwonminha
+ * @date 2020. 4. 7.
+ * 스택 수열 
+ * https://www.acmicpc.net/problem/1874
+ */
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
 
-		System.out.println("git test");
+		Stack<Integer> st = new Stack<Integer>();
+		int countNum = 0;
+
+		for(int i = 0; i < n; i++) {
+			int inputNum = sc.nextInt();
+			
+			if(!st.isEmpty() && inputNum == st.peek()) {
+				st.pop();
+				System.out.println("-");
+			}
+			
+			
+
+
+		}
 	}
 
 }
