5c7b38f9932e5df3a2fdcf5903d1b0096edbba2a
Minha Kwon <minha61@naver.com>
2020-04-07 22:22:11 +0900

    BOJ#1874 - 스택수열

--- BOJ#1874/src/Main.java
+++ BOJ#1874/src/Main.java
@@ -1,12 +1,11 @@
-import java.util.Scanner;
-import java.util.Stack;
-
 /**
  * @author gwonminha
  * @date 2020. 4. 7.
  * 스택 수열 
  * https://www.acmicpc.net/problem/1874
  */
+import java.util.*;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -14,20 +13,30 @@ public class Main {
 		int n = sc.nextInt();
 
 		Stack<Integer> st = new Stack<Integer>();
+		StringBuilder sb = new StringBuilder();
 		int countNum = 0;
 
 		for(int i = 0; i < n; i++) {
 			int inputNum = sc.nextInt();
-			
+			int gap = inputNum - countNum;
+			if(countNum < inputNum) {
+				for(int j = 0; j < gap; j++) {
+					countNum++;
+					st.push(countNum);
+					sb.append("+\n");
+				}
+			}
+
 			if(!st.isEmpty() && inputNum == st.peek()) {
 				st.pop();
-				System.out.println("-");
+				sb.append("-\n");
+			} else {
+				System.out.println("NO");
+				return;
 			}
-			
-			
-
-
 		}
+		
+		System.out.println(sb.toString());
 	}
 
 }
