b618f978cc9e6c7074d0db8dd3121bf047441784
Minha Kwon <minha61@naver.com>
2020-04-29 19:04:39 +0900

    BOJ#10799
    Add comment

--- BOJ#10799/src/Main.java
+++ BOJ#10799/src/Main.java
@@ -31,13 +31,13 @@ public class Main {
 					isVPS = true;
 				
 				} else { //쇠막대기 끝나는 괄호 )
-					stack.pop();
-					count++;
+					stack.pop(); //하나의 쇠막대기가 끝났으닌까 pop()
+					count++; // 그리고 1 추가 - 왜?  
 				}
 			}
-			if(stack.isEmpty()) {
-				result += count;
-				count = 0;
+			if(stack.isEmpty()) { //쇠막대기 제일 긴게 끝났을 때(그럼 스택엔 아무것도 없겠져?)
+				result += count; //지금까지 잘린 조각 최종 result에 저장 
+				count = 0; // 다시 나올 새로운 쇠막대기를 위해 0으로 리셋 
 			}
 		}
 		
