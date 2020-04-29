dcc4fb00196492523d5d67012ff55848e5b200cb
Minha Kwon <minha61@naver.com>
2020-04-29 19:41:48 +0900

    BOJ#10799
    Solved!

--- BOJ#10799/src/Main.java
+++ BOJ#10799/src/Main.java
@@ -16,7 +16,6 @@ public class Main {
 		Stack<Character> stack = new Stack<Character>();
 
 		int result = 0;
-		int count = 0;
 		
 		boolean isVPS = true;
 		
@@ -24,20 +23,16 @@ public class Main {
 			if(str.charAt(i) == '(') {
 				isVPS = false;
 				stack.push('(');
-			} else { // ) 닫는 괄호 만났을 때 
+			} else { //')' 닫는 괄호 만났을 때 
 				if(isVPS == false) { //레이저로 잘릴 때 () 
 					stack.pop();
-					count += stack.size();
+					result += stack.size();
 					isVPS = true;
 				
 				} else { //쇠막대기 끝나는 괄호 )
-					stack.pop(); //하나의 쇠막대기가 끝났으닌까 pop()
-					count++; // 그리고 1 추가 - 왜?  
+					stack.pop(); //하나의 쇠막대기가 끝났으닌까, 끝나면서 한 조각이 남으닌까 +1 
+					result++; 
 				}
-			}
-			if(stack.isEmpty()) { //쇠막대기 제일 긴게 끝났을 때(그럼 스택엔 아무것도 없겠져?)
-				result += count; //지금까지 잘린 조각 최종 result에 저장 
-				count = 0; // 다시 나올 새로운 쇠막대기를 위해 0으로 리셋 
 			}
 		}
 		
