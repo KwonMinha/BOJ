2b338562a3b264b7b95511f7ad64fe627a94327c
Minha Kwon <minha61@naver.com>
2020-05-03 20:18:16 +0900

    BOJ#10430
    Solved

--- BOJ#10430/src/Main.java
+++ BOJ#10430/src/Main.java
@@ -18,7 +18,7 @@ public class Main {
 		System.out.println((A+B)%C);
 		System.out.println(((A%C) + (B%C))%C);
 		System.out.println((A*B)%C);
-		System.out.println(((A%C)*(B%C))%C);
+		System.out.println(((A%C)*(B%C))%C); 
 	}
 
 }
