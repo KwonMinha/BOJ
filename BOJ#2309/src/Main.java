62500557f7b6222f51cfb8c42cf587e3a737f46d
Minha Kwon <minha61@naver.com>
2020-05-20 23:40:22 +0900

    Create BOJ#2309

---
+++ BOJ#2309/src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * @author Minha Gwon
+ * @date 2020. 5. 20.
+ * 일곱 난쟁이 
+ * https://www.acmicpc.net/problem/2309
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
