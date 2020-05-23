747376041d0799d8addf5e5bde4d1a3a907d5bcb
Minha Kwon <minha61@naver.com>
2020-05-24 03:58:37 +0900

    Solved
    기존 시간초과 코드에서 x1를 M 값으로 고정 후
    y1에 계속 M만큼 추가하며 x== x1, y == y1을 찾음.
    그리고 최소 공배수 적용안하고 그냥 정답 ans 값이 M*N을  초과하면 구할 수 없다고 판단하고 -1 출력

--- BOJ#6064/src/Main.java
+++ BOJ#6064/src/Main.java
@@ -1,11 +1,12 @@
-import java.util.Scanner;
-
 /**
  * @author Minha Gwon
  * @date 2020. 5. 24.
  * 카잉달력
  * https://www.acmicpc.net/problem/6064
  */
+
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -17,29 +18,32 @@ public class Main {
 			int x = sc.nextInt();
 			int y = sc.nextInt();
 			
-			int ans = 1;
-			int x1 = 1;
-			int y1 = 1;
+			int ans = x;
+			int x1 = x;
+			int y1 = x;
+			int cnt = 1;
 			
-			while(x1 != x || y1 != y) {
-				ans++;
-				x1++;
-				y1++;
-				
-				if(x1 >= M && y1 == N)
-					break;
-				
-				if(x1 == M)
-					x1 = 0;
-				
-				if(y1 == N)
-					y1 = 0;
+			if(y1 > N) {
+				while(y1 > N)
+					y1 -= N;
 			}
 			
-			if(x == x1 && y == y1)
-				System.out.println(ans);
-			else
-				System.out.println(-1);	
+			while(x1 != x || y1 != y) {
+				//System.out.println(x1 + ", " + y1);
+				if(ans > M*N) {
+					ans = -1;
+					break;
+				}
+				cnt++;
+				ans += M;
+				y1 += M;
+				
+				if(y1 > N) {
+					while(y1 > N)
+						y1 -= N;
+				}
+			}
+			System.out.println(ans);	
 		}
 	}
 
