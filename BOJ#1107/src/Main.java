d14c2eecbd001f0c0c9350925cc010a9bf9b12c3
Minha Kwon <minha61@naver.com>
2020-05-23 22:32:49 +0900

    BOJ#1107
    리모콘

---
+++ BOJ#1107/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * @author Minha Gwon
+ * @date 2020. 5. 23.
+ * 리모콘  
+ * https://www.acmicpc.net/problem/1107
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
