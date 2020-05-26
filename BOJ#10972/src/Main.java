dff98c720c9d00aec3b76436477558094b3bdee5
Minha Kwon <minha61@naver.com>
2020-05-26 23:58:29 +0900

    BOJ#10972
    Failure - Time Out
    (BufferedReader / BufferedWriter / StringBuilder Version)

--- BOJ#10972/src/Main.java
+++ BOJ#10972/src/Main.java
@@ -5,22 +5,33 @@
  * https://www.acmicpc.net/problem/10972
  */
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 	static int[] input;
 	static boolean flag = false;
 	static int factorial = 1;
 	static int count = 0;
+	static StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		
+		int N = Integer.parseInt(br.readLine());
 		input = new int[N];
 		int[] arr = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < N; i++) {
-			input[i] = sc.nextInt();
+			input[i] = Integer.parseInt(st.nextToken());
 			arr[i] = i + 1;
 			factorial *= (i+1);
 		}
@@ -30,6 +41,10 @@ public class Main {
 		
 		per(arr, v, N, 0, output);
 		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		
 	}
 
 	public static void per(int[] arr, boolean[] v, int n, int r, int[] output) {
@@ -38,14 +53,14 @@ public class Main {
 			
 			if(flag) {
 				for(int i = 0; i < output.length; i++) {
-					System.out.print(output[i] + " ");
+					sb.append(output[i] + " ");
 				}
-				System.exit(0);
+				flag = false;
 			}
 			
 			if(Arrays.equals(input, output)) {
 				if(count == factorial) {
-					System.out.println(-1);
+					sb.append(-1);
 				} else {
 					flag = true;
 				}
