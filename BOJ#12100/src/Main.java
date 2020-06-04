24c3b03169efdeefdd67b658f69513c38b623701
Minha Kwon <minha61@naver.com>
2020-06-04 23:13:33 +0900

    BOJ#12100
    Not solved - Run Time Error

--- BOJ#12100/src/Main.java
+++ BOJ#12100/src/Main.java
@@ -1,15 +1,25 @@
+/**
+ * @author Minha Gwon
+ * @date 2020. 6. 4.
+ * 2048(Easy)
+ * https://www.acmicpc.net/problem/12100
+ */
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static int[][] board;
-	private static int max = 0;
+	private static long[][] board;
+	private static long max = 0;
 	private static int n;
-	private static int[] output;
+	private static long[] output;
+	private static LinkedList<Long> boardList;
+	private static LinkedList<Long> num;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,8 +28,8 @@ public class Main {
 
 		int N = Integer.parseInt(st.nextToken());
 		n = N;
-		board = new int[N][N];
-		output = new int[5];
+		board = new long[N][N];
+		output = new long[5];
 
 		for(int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
@@ -27,14 +37,8 @@ public class Main {
 				board[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-
-		output[0] = 2;
-		output[1] = 3;
-		output[2] = 0;
-		output[3] = 1;
-		output[4] = 2;
-
-		moveBlock();
+		
+		dfs(0);
 
 		bw.write(max + "");	
 		bw.flush();
@@ -56,260 +60,117 @@ public class Main {
 
 	public static void moveBlock() {
 		for(int i = 0; i < output.length; i++) {
-			int dir = output[i];
+			long dir = output[i];
 
-			switch(dir) {
-			case 0:
-				//위로 이동 
-				System.out.println("위로 이동");
-				for(int j = 0; j < n; j++) {
-					String s = "";
+			for(int j = 0; j < n; j++) {
+				boardList = new LinkedList<Long>();
+				for(int k = 0; k < n; k++) {
+					if(dir == 0 || dir == 1) 
+						boardList.add(board[k][j]);
+					else
+						boardList.add(board[j][k]);
+				}
 
-					for(int k = 0; k < n; k++) {
-						s += board[k][j];
-					}
-
-					s = s.replace("0", "");
-
-					while(s.length() != n)
-						s += "0";
-
-					for(int k = 0; k < n; k++) {
-						board[k][j] = Integer.parseInt(s.charAt(k)+"");
+				for(int k = 0; k < boardList.size(); k++) {
+					if(boardList.get(k) == 0) {
+						boardList.remove(k);
+						k--;
 					}
 				}
-				break;
-			case 1:
-				//아래로 이동
-				System.out.println("아래로 이동");
-				for(int j = 0; j < n; j++) {
-					String s = "";
 
-					for(int k = 0; k < n; k++) {
-						s += board[k][j];
-					}
-
-					s = s.replace("0", "");
-
-					while(s.length() != n)
-						s  = "0" + s;
-
-					for(int k = 0; k < n; k++) {
-						board[k][j] = Integer.parseInt(s.charAt(k)+"");
-					}
+				//이동하며 0을 삭제한 만큼 0 채워주기 
+				if(dir == 0 || dir == 2) {
+					while(boardList.size() != n)
+						boardList.addLast((long) 0);
+				} else {
+					while(boardList.size() != n)
+						boardList.addFirst((long) 0);
 				}
-				break;
-			case 2:
-				//왼쪽으로 이동 
-				System.out.println("왼쪽으로 이동");
-				for(int j = 0; j < n; j++) {
-					String s = "";
 
-					for(int k = 0; k < n; k++) {
-						s += board[j][k];
-					}
-
-					s = s.replace("0", "");
-
-					while(s.length() != n)
-						s += "0";
-
-					for(int k = 0; k < n; k++) {
-						board[j][k] = Integer.parseInt(s.charAt(k)+"");
-					}
+				//board에 이동 완료한 값 넣어주기 
+				if(dir == 0 || dir == 1) {
+					for(int k = 0; k < n; k++)
+						board[k][j] = boardList.get(k);
+				} else {
+					for(int k = 0; k < n; k++)
+						board[j][k] = boardList.get(k);
 				}
-				break;
-			case 3:
-				//오른쪽으로 이동 
-				System.out.println("오른쪽으로 이동");
-				for(int j = 0; j < n; j++) {
-					String s = "";
-
-					for(int k = 0; k < n; k++) {
-						s += board[j][k];
-					}
-
-					s = s.replace("0", "");
-
-					while(s.length() != n)
-						s = "0" + s;
-
-					for(int k = 0; k < n; k++) {
-						board[j][k] = Integer.parseInt(s.charAt(k)+"");
-					}
-				}
-				break;
-			default:
-				break;
 			}
+			
+			//move후 board의 상태 출력 
+			System.out.println(dir + " move");
+			print();
 
-
-
-
-
+			//상하좌우 모든 숫자 값 이동 후 블록 합치기 merge
 			mergeBlock(dir);
 
+			//merge후 board의 상태 출력 
+			System.out.println(dir + " merge");
+			print();
+
+			//board의 모든 줄을 다 이동시키고 merge했다면 max 구하기 
 			if(i == output.length-1) {
 				getMax();
 			}
 		}
 	}
 
-	public static void mergeBlock(int dir) {
-		switch(dir) {
-		case 0:
-			for(int i = 0; i < n; i++) {
+	public static void mergeBlock(long dir) {
+		//한 줄씩 merge해주기 위해 값 가져와서 LinkedList에 저장 
+		for(int i = 0; i < n; i++) {
+			num = new LinkedList<Long>();
 
-				String s = "";
-				for(int j = 0; j < n; j++) {
-					s += board[j][i];
-				}
-
-				boolean flag = true;
-				while(flag) {
-					for(int k = 0; k < s.length()-1; k++) {
-						if(s.charAt(k) != '0' && s.charAt(k) == s.charAt(k+1)) {
-							flag = false;
-							int val = Integer.parseInt(s.charAt(k) + "");
-							String s1 = s.substring(0, k);
-							String s2 = s.substring(k+2, s.length());
-							s = s1 + val*2 + s2;
-						}
-					}
-
-					if(flag = false) {
-						flag = true;
-					}
-				}
-
-				while(s.length() != n) {
-					s += "0";
-				}
-
-				for(int k = 0; k < n; k++) {
-					board[k][i] = Integer.parseInt(s.charAt(k)+"");
-				}
-			}
-			break;
-		case 1:
-			//System.out.println("merge");
-			for(int i = 0; i < n; i++) {
-				String s = "";
-				for(int j = 0; j < n; j++) {
-					s += board[j][i];
-				}
-
-				//System.out.println("s : " + s);
-
-				boolean flag = true;
-				while(flag) {
-					for(int k = s.length()-1; k > 0; k--) {
-						if(s.charAt(k) != '0' && s.charAt(k) == s.charAt(k-1)) {
-							flag = false;
-							int val = Integer.parseInt(s.charAt(k) + "");
-							String s1 = s.substring(k+1, s.length());
-							String s2 = s.substring(0, k-1);
-
-							//System.out.println(s1 + " " + val*2 +  " " + s2);
-							s = s2 + val*2 + s1;
-							//k--;
-						}
-					}
-
-					if(flag = false) {
-						flag = true;
-					}
-				}
-
-				while(s.length() != n) {
-					s = "0" + s;
-				}
-
-				for(int k = 0; k < n; k++) {
-					board[k][i] = Integer.parseInt(s.charAt(k)+"");
-				}
-			}
-			break;
-		case 2:
-			for(int i = 0; i < n; i++) {
-				LinkedList<Integer> num = new LinkedList<Integer>();
-
-				String s = "";
-				for(int j = 0; j < n; j++) {
-					s += board[i][j];
+			for(int j = 0; j < n; j++) {
+				if(dir == 0 || dir == 1)
+					num.add(board[j][i]);
+				else
 					num.add(board[i][j]);
-				}
+			}
 
-				boolean flag = true;
-				while(flag) {
-					for(int k = 0; k < num.size()-1; k++) {
-						if(num.get(k) != 0 && num.get(k) == num.get(k+1)) {
-							flag = false;
-							int val = num.get(k)*2;
-							num.remove(k);
-							num.remove(k);
-							num.addFirst(val);
-						}
-
-						if(flag = false)
-							flag = true;
+			//merge 
+			if(dir == 0 || dir == 2) {
+				for(int k = 0; k < num.size()-1; k++) {
+					if(!num.get(k).equals(0) && num.get(k).equals(num.get(k+1))) {
+						long val = num.get(k)*2;
+						num.remove(k);
+						num.remove(k);
+						num.add(k, val);
 					}
 				}
-				
-				while(num.size() != n)
-					num.addLast(0);
+			} else {
+				for(int k = num.size()-1; k > 0; k--) {
+					if(!num.get(k).equals(0) && num.get(k).equals(num.get(k-1))) {
+						long val = num.get(k)*2;
+						num.remove(k);
+						num.remove(k);
+						num.add(k-1, val);
+					}
+				}
+			}	
 
+			//merge 과정에서 줄어든 자리수 맞춰 주기 
+			if(dir == 0 || dir == 2) {
+				while(num.size() != n)
+					num.addLast((long) 0);
+			} else {
+				while(num.size() != n)
+					num.addFirst((long) 0);
+			}
+
+			//merge한 값 배열에 넣어주기 
+			if(dir == 0 || dir == 1) {
+				for(int k = 0; k < n; k++) {
+					board[k][i] = num.get(k);
+				}
+			} else {
 				for(int k = 0; k < n; k++) {
 					board[i][k] = num.get(k);
 				}
 			}
-			break;
-		case 3:
-			for(int i = 0; i < n; i++) {
-				String s = "";
-				for(int j = 0; j < n; j++) {
-					s += board[i][j];
-				}
-
-				//System.out.println("s : " + s);
-
-				boolean flag = true;
-				while(flag) {
-					for(int k = s.length()-1; k > 0; k--) {
-						if(s.charAt(k) != '0' && s.charAt(k) == s.charAt(k-1)) {
-							flag = false;
-							int val = Integer.parseInt(s.charAt(k) + "");
-							String s1 = s.substring(k+1, s.length());
-							String s2 = s.substring(0, k-1);
-
-							//System.out.println(s1 + " " + val*2 +  " " + s2);
-							s = s2 + val*2 + s1;
-							//k--;
-						}
-					}
-
-					if(flag = false) {
-						flag = true;
-					}
-				}
-
-				while(s.length() != n) {
-					s = "0" + s;
-				}
-
-				for(int k = 0; k < n; k++) {
-					board[i][k] = Integer.parseInt(s.charAt(k)+"");
-				}
-			}
-			break;
-		default:
-			break;
 		}
-
-		print();
-
 	}
 
+	//board 출력 함수 
 	public static void print() {
 		for(int i = 0; i < n; i++) {
 			for(int j = 0; j < n; j++) {
@@ -320,15 +181,12 @@ public class Main {
 		System.out.println();
 	}
 
+	//board에 있는 값들을 전부 합해 max를 구하는 함수 
 	public static void getMax() {
-		int cnt = 0;
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
-				cnt += board[i][j];
-			}
-		}
-
-		max = Math.max(cnt, max);
+		for(int i = 0; i < n; i++) 
+			for(int j = 0; j < n; j++) 
+				max = Math.max(board[i][j], max);
 	}
+
 }
 
