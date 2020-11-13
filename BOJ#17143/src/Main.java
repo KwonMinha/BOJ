da49732740a9dee16cf4ded5513dab4d78cb3803
Minha Kwon <minha61@naver.com>
2020-11-14 07:03:43 +0900

    낚시왕
    Solved

--- BOJ#17143/src/Main.java
+++ BOJ#17143/src/Main.java
@@ -1,154 +1,128 @@
 /**
  * @author Minha Gwon
- * @date 2020. 6. 16.
- * 낚시왕
- * https://www.acmicpc.net/problem/17143
+ * 2020. 10. 12.
+ * URL : https://www.acmicpc.net/problem/17143
+ * BLOG : https://minhamina.tistory.com/65
+ * BOJ#17143 - 낚시왕 
  */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static int R, C, M;
-	public static ArrayList<Shark> shark;
-	public static int[][] map;
-	public static int ans = 0;
+	public static Shark[][] map;
+	public static int answer = 0;
+	public static int dx[] = {-1, 0, 1, 0}; //상 좌 하 우 순 
+	public static int dy[] = {0, -1, 0, 1};
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		R = sc.nextInt();
-		C = sc.nextInt();
-		M = sc.nextInt();
-		map = new int[R][C];
-		shark = new ArrayList<>();
+	public static void main(String[] args) throws IOException {
+		//입력 받기 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		R = Integer.parseInt(st.nextToken()); // 행의 수
+		C = Integer.parseInt(st.nextToken()); // 열의 수
+		M = Integer.parseInt(st.nextToken()); // 상어의 수
+
+		// 상어 낚시 격자판 만들고, 각 위치에 상어 클래스로 만든 인스턴스 저장 
+		map = new Shark[R][C];
 		for(int i = 0; i < M; i++) {
-			int r = sc.nextInt();
-			int c = sc.nextInt();
-			int s = sc.nextInt();
-			int d = sc.nextInt();
-			int z = sc.nextInt();
-			shark.add(new Shark(r-1, c-1, s, d, z));
-			map[r-1][c-1] = z;
+			st = new StringTokenizer(br.readLine(), " ");
+			int r = Integer.parseInt(st.nextToken()); // 행 위치 
+			int c = Integer.parseInt(st.nextToken()); // 열 위치 
+			int s = Integer.parseInt(st.nextToken()); // 속력 
+			int d = Integer.parseInt(st.nextToken()); // 이동 방향 
+			int z = Integer.parseInt(st.nextToken()); // 크기 
+ 
+			// 방향 쉽게 바꾸기위해 입력받은 상하좌우(1 2 3 4) -> 상좌하우(0 1 2 3)로 변경 
+			if(d == 1)
+				d = 0;
+			else if(d == 4)
+				d = 1;
+            
+			map[r-1][c-1] = new Shark(r-1, c-1, s, d, z); // 격자판에 상어 저장 
 		}
 
-		Collections.sort(shark);
-
-		for(int i = 0; i < C; i++) {
-			Collections.sort(shark);
-			for(int j = 0; j < shark.size(); j++) {
-				Shark s = shark.get(j);
-				if(s.c == i) {
-					ans += s.z;
-					map[s.r][s.c] = 0;
-					shark.remove(s); 
+		
+		for(int col = 0; col < C; col++) { // 열의 끝까지 반복 
+			// 1. 낚시왕 이동 
+			for(int row = 0; row < R; row++) {
+				if(map[row][col] != null) { 
+					answer += map[row][col].z; // 2. 가장 가까운 상어 크기 정답 변수에 저장 
+					map[row][col] = null; // map에서 상어 없애기 
 					break;
 				}
 			}
-			move();
-		}
-		System.out.println(ans);
-	}
 
-	public static void printMap() {
-		for(int i = 0; i < R; i++) {
-			for(int j = 0; j < C; j++)
-				System.out.print(map[i][j] + " ");
-			System.out.println();
-		}
-		System.out.println();
-	}
+			// 3. 상어 이동 
+			Queue<Shark> queue = new LinkedList<>(); 
+			for(int i = 0; i < R; i++) {
+				for(int j = 0; j < C; j++) {
+					if(map[i][j] != null) { // 현재 map에 있는 상어들 큐에 추가 
+						queue.add(new Shark(i, j, map[i][j].s, map[i][j].d, map[i][j].z));
+					}
+				}
+			}
 
-	public static void move() {
-		for(int[] i : map) 
-			Arrays.fill(i, 0);
+			map = new Shark[R][C]; // 새로운 낚시판 만들기위해 배열 초기화 
 
-		for(int x = 0; x < shark.size(); x++) {
-			Shark s = shark.get(x);
-			int nr, nc, nd;
-
-			//상어 이동 
-			if(s.d == 1 || s.d == 2) { //상어의 방향이 위, 아래일 경우 
-				nr = s.r;
-				nc = s.c;
-				nd = s.d;
+			// 모든 상어 한마리씩 꺼내서 이동 
+			while(!queue.isEmpty()) {
+				Shark sm = queue.poll();
+                
+				// 속력만큼 상어 이동 시키기 
+				int speed = sm.s; // 시간초과로 최소한의 이동을 위해 나머지 연산
+				if(sm.d == 0 || sm.d == 2) //상 하
+					speed %= (R -1) * 2; 
+				else if(sm.d == 1 || sm.d == 3) //좌 우
+					speed %= (C -1) * 2;
 				
-				int speedCnt = s.s;
-				while(speedCnt > 0) {
-					if(nd == 1) {
-						nr--;
-						if(nr < 0) {
-							nr++;
-							nr++;
-							nd = 2;
-						}
-					} else {
-						nr++;
-						if(nr >= R) {
-							nr--;
-							nr--;
-							nd = 1;
-						}
-					}
-					speedCnt--;
-				}		
-			} else { //상어의 방향이 왼쪽, 오른쪽일 경우 
-				nr = s.r;
-				nc = s.c;
-				nd = s.d;
+				for(int s = 0; s < speed; s++) {
+					// 현재 r, c에 방향에 맞게 1칸씩 추가하며 위치 이동 
+					int newR = sm.r + dx[sm.d]; 
+					int newC = sm.c + dy[sm.d];
 
-				int speedCnt = s.s;
-				while(speedCnt > 0) {
-					if(nd == 4) {
-						nc--;
-						if(nc < 0) {
-							nc++;
-							nc++;
-							nd = 3;
-						}
-					} else {
-						nc++;
-						if(nc >= C) {
-							nc--;
-							nc--;
-							nd = 4;
-						}
+					// 이동할 새로운 위치가 범위를 벗어나 벽에 부딪히면 
+					if(newR < 0 || newR >= R || newC < 0 || newC >= C) { 
+						sm.r -= dx[sm.d]; // 다시 값 돌려주고 
+						sm.c -= dy[sm.d];
+						sm.d = (sm.d + 2) % 4; // 방향 반대로 
+						continue;
 					}
-					speedCnt--;
+
+					// 위치 벗어나지 않을때는 새로운 위치로 이동 
+					sm.r = newR; 
+					sm.c = newC;
+				}
+
+				// 4. 새로운 위치가 빈 공간인지 이미 상어가 있는지 확인
+				if(map[sm.r][sm.c] != null) { // 이미 상어가 있다면 두 상어 크기 비교 
+					if(map[sm.r][sm.c].z < sm.z) { // 기존 상어보다 현재 상어가 크다면 
+						map[sm.r][sm.c] = new Shark(sm.r, sm.c, sm.s, sm.d, sm.z); // 현재 상어 넣어줌 
+					} 
+				} else { // 없다면 현재 상어 바로 넣어줌 
+					map[sm.r][sm.c] = new Shark(sm.r, sm.c, sm.s, sm.d, sm.z);
 				}
 			}
+		} // 이동 for문 끝 
 
-			//이동한 자리가 빈칸인지 이미 다른 상어가 있는지 확인 
-			if(map[nr][nc] == 0) { //빈칸 0일 경우 
-				map[nr][nc] = s.z;
-				s.r = nr;
-				s.c = nc;
-				s.d = nd;
-			} else { //이미 다른 상어가 있는 경우 
-				if(map[nr][nc] < s.z) { //현재 상어가 이전의 상어보다 크기가 큰 경우 > 이전 상어가 먹힘 
-					for(int i = 0; i < shark.size(); i++) {
-						if(shark.get(i).r == nr && shark.get(i).c == nc) {
-							shark.remove(i);
-							x--;
-						}
-					}
-					map[nr][nc] = s.z;
-					s.r = nr;
-					s.c = nc;
-					s.d = nd;
-				} else { //크기가 작은 경우 > 현재 상어가 먹힘 
-					shark.remove(s);
-					x--;
-				}
-			}
-		}
+		System.out.println(answer);
 	}
-
 }
 
-class Shark implements Comparable<Shark> {
-	int r, c, s, d, z;
+//상어 정보를 저장할 상어 클래스 
+class Shark {
+	int r;
+	int c;
+	int s;
+	int d;
+	int z;
 
 	Shark(int r, int c, int s, int d, int z) {
 		this.r = r;
@@ -156,17 +130,5 @@ class Shark implements Comparable<Shark> {
 		this.s = s;
 		this.d = d;
 		this.z = z;
-	}
-
-	@Override
-	public int compareTo(Shark p) {
-		if(this.c > p.c) {
-			return 1;
-		} else if(this.c == p.c) {
-			if(this.r > p.r) {
-				return 1;
-			}
-		}
-		return -1;
 	}
 }
