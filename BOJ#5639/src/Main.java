e93f5e9e51be21733643bd0c90b688d9b1e0b579
Minha Kwon <minha61@naver.com>
2021-01-02 22:16:06 +0900

    이진 탐색 트리
    insertNode
    insrtNode2
    insert
    
    삽입 메소드 총 3개
    비슷한 듯 다름 - 정리 필요

--- BOJ#5639/src/Main.java
+++ BOJ#5639/src/Main.java
@@ -4,36 +4,91 @@ class Node {
 	int data;
 	Node left;
 	Node right;
-	
+
 	Node(int data) {
 		this.data = data;
 	}
- }
+}
 
 
 public class Main {
-	public Node root;
-	
-	public void createNode(Node node, int key) {
-		if(root == null) {
-			root = new Node(key);
-		} else {
-			if(key < node.data) {
-				//node.left = createNode(node.left, key);
-			} else if(key > node.data) {
-				//node.right = createNode(node.right, key);
-				
-			}
+	public static Node root;
+
+	public static Node insertNode(Node node, int key) {
+		if(node == null) {
+			return createNode(key);
 		}
-		
+
+		if(key < node.data) {
+			node.left = insertNode(node.left, key);
+		} else if(key > node.data) {
+			node.right = insertNode(node.right, key);
+		}
+
+		return node;
+	}
+	
+	public static Node insertNode2(Node node, int key) {
+		if(node == null) {
+			root = new Node(key);
+			return root;
+			
+//			node = new Node(key);
+//			return node;
+		}
+
+		if(key < node.data) {
+			node.left = insertNode2(node.left, key);
+		} else if(key > node.data) {
+			node.right = insertNode2(node.right, key);
+		}
+
+		return node;
+	}
+
+	public static Node insert(Node root, int key) {
+		Node parent = root;
+		Node newNode = new Node(key);
+
+		if(parent ==  null) {
+			return newNode;
+		} else if(parent.data > newNode.data) {
+			parent.left = insert(parent.left, key);
+			return parent;	
+		} else if(parent.data < newNode.data) {
+			parent.right = insert(parent.right, key);
+			return parent;	
+		} else {
+			return parent;
+		}
+	}
+
+	public static Node createNode(int key) {
+		Node node = new Node(key);
+		return node;
+	}
+
+	//후위순회 Postorder : Left -> Right -> Root
+	public void postOrder(Node node) {
+		if(node != null) {
+			if(node.left != null) postOrder(node.left);
+			if(node.right != null) postOrder(node.right);
+			System.out.println(node.data);
+		}
 	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		Main m = new Main();
+
 		while(sc.hasNext()) {
-			
+			//m.root = insertNode2(m.root, Integer.parseInt(sc.nextLine()));
+			m.root = insert(m.root, Integer.parseInt(sc.nextLine()));
 		}
+		
+		// mac북에서 ctrl + d 누르면 입력받기 종료 
+		m.postOrder(m.root);
 	}
 
 }
