bfba9f9592a71ba6023297fc29c6d49c3a8e9081
Minha Kwon <minha61@naver.com>
2021-01-02 22:19:11 +0900

    Create Main.java

---
+++ BOJ#2533/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * @author Minha Gwon
+ * @date 2021. 1. 2.
+ * 사회망 서비스 (SNS))
+ * https://www.acmicpc.net/problem/2533 
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
