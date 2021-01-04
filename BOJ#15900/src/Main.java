a2f4cc396e6e0d04b3181b0e84cb455ae9ca63b9
Minha Kwon <minha61@naver.com>
2021-01-05 02:44:03 +0900

    나무 탈출
    Solved

--- BOJ#15900/src/Main.java
+++ BOJ#15900/src/Main.java
@@ -3,6 +3,7 @@
  * @date 2021. 1. 5.
  * 나무 탈출 
  * https://www.acmicpc.net/problem/15900
+ * BLOG - 
  */
 
 import java.io.BufferedReader;
@@ -13,7 +14,6 @@ import java.util.StringTokenizer;
 
 public class Main {
 	static LinkedList<Integer>[] adjList;
-	static boolean[] visited;
 	public static int answer = 0;
 
 	public static void main(String[] args) throws IOException {
@@ -21,11 +21,7 @@ public class Main {
 		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st;
 		
-		
-		//Scanner sc = new Scanner(System.in);
-		//int N = sc.nextInt();
 		adjList = new LinkedList[N+1];
-		visited = new boolean[N+1];
 
 		for (int i = 1; i <= N; i++) {
 			adjList[i] = new LinkedList<Integer>();
@@ -33,18 +29,15 @@ public class Main {
 
 		for(int i = 0; i < N-1; i++) {
 			st = new StringTokenizer(br.readLine());
-
+			
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			
-//			int a = sc.nextInt();
-//			int b = sc.nextInt();
 
 			adjList[a].add(b);
 			adjList[b].add(a);
 		}
 
-		dfs(1, 0, 0); // 각 노드의 부모 노드 구하기 
+		dfs(1, 0, 0);
 		
 		System.out.println((answer % 2) == 0 ? "No" : "Yes");
 		
@@ -58,7 +51,7 @@ public class Main {
 			}
 		}
 		
-		if(cur != 1 && adjList[cur].size() == 1) {
+		if(adjList[cur].size() == 1) {
 			answer += cnt;
 		}
 	}
