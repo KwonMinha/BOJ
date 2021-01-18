7f7b161b56e58625ce52a497a574a898e680a541
Minha Kwon <minha61@naver.com>
2021-01-19 02:02:14 +0900

    결혼식
    Solved

--- BOJ#5567/src/Main.java
+++ BOJ#5567/src/Main.java
@@ -11,28 +11,29 @@ public class Main {
 	public static ArrayList<Integer>[] adjList;
 	public static int[] visited;
 	public static int ans = 0;
-	
+
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		int M = sc.nextInt();
-		
+
 		adjList = new ArrayList[N + 1];
 		visited = new int[N + 1];
-	
+
 		for(int i = 1; i <= N; i++) {
 			adjList[i] = new ArrayList<Integer>();
 		}
-		
+
 		for(int i = 0; i < M; i++) {
 			int a = sc.nextInt();
 			int b = sc.nextInt();
 			adjList[a].add(b);
 			adjList[b].add(a);
 		}
-		
+
 		bfs();
+		
 		System.out.println(ans);
 	}
 
@@ -40,18 +41,18 @@ public class Main {
 		Queue<Integer> queue = new LinkedList<>();
 		queue.add(1);
 		visited[1]++;
-		
+
 		while(!queue.isEmpty()) {
 			int v = queue.poll();
-			
-			if(visited[v] > 3) {
+
+			if(visited[v] >= 3) {
 				break;
 			}
-			
-			for(int i = 0; i < adjList[v].size(); i++) {
-				if(visited[adjList[v].get(i)] == 0) {
-					visited[adjList[v].get(i)] = visited[v]+1;
-					queue.add(adjList[v].get(i));
+
+			for(int next : adjList[v]) {
+				if(visited[next] == 0) {
+					visited[next] = visited[v] + 1;
+					queue.add(next);
 					ans++;
 				}
 			}
