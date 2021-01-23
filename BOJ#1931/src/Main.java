8fe8181aa763ffc039af5e4dbe7522c8ff546c8b
Minha Kwon <minha61@naver.com>
2021-01-23 21:12:54 +0900

    Create Main.java

---
+++ BOJ#1931/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * @author Minha Gwon
+ * @date 2021. 1. 23.
+ * 회의실 배정
+ * https://www.acmicpc.net/problem/1931
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
