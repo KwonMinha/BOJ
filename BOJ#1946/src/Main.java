6cfba208fac524e6193034d5bbe7e6b1dd512aea
Minha Kwon <minha61@naver.com>
2021-01-25 05:14:15 +0900

    신입 사원
    Solved

--- BOJ#1946/src/Main.java
+++ BOJ#1946/src/Main.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
 class Grade implements Comparable<Grade> {
 	int a;
 	int b;
-	
+
 	Grade(int a, int b) {
 		this.a = a;
 		this.b = b;
@@ -22,12 +22,6 @@ class Grade implements Comparable<Grade> {
 	public int compareTo(Grade o) {
 		if(this.a > o.a) {
 			return 1;
-		} else if(this.a == o.a) {
-			if(this.b > o.b) {
-				return 1;
-			} else {
-				return -1;
-			}
 		} else {
 			return -1;
 		}
@@ -39,45 +33,28 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
-		
+
 		for(int t = 0; t < T; t++) {
 			int N = sc.nextInt();
 			ArrayList<Grade> list = new ArrayList<>();
-			
+
 			for(int i = 0; i < N; i++) {
 				int a = sc.nextInt();
 				int b = sc.nextInt();
-				
+
 				list.add(new Grade(a, b));
 			}
-			
+
 			Collections.sort(list); // 서류 점수 기준 오름차순 정렬 
-			
-			for(Grade g : list) {
-				System.out.println(g.a + " " + g.b);
-			}
-		
+
 			int ans = 1; // 서류 1등은 무조건 통과 
-			
-			for(int i = 1; i < N; i++) { // 0번 인덱스 1등은 통과했으니 1번 인덱스 부터 시작 
-				int a1 = list.get(i).a;
-				int b1 = list.get(i).b;
-				
-				boolean flag = true;
-				for(int j = 0; j < i; j++) {
-					int a2 = list.get(j).a;
-					int b2 = list.get(j).b;
-					 
-					if(b1 > b2) { // 현재 인덱스 이전의 서류 등수 높은 애들보다 면접은 좋아야함 
-						flag = false;
-						break;
-					}
-				}
-				
-				if(flag) 
+			int min = list.get(0).b; // 면접 점수 최소값 
+			for(int i = 1; i < N; i++) { // 서류 2등부터 시작 
+				if(list.get(i).b < min) { // 이전의 최소 면접 점수보다 낮으면 통과 
 					ans++;
+					min = list.get(i).b; // 최소 점수 갱신 
+				}
 			}
-			
 			System.out.println(ans);
 		}	
 	}
