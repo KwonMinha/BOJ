b324c8d038f461117c5cf53a924f1de981ca296c
Minha Kwon <minha61@naver.com>
2021-01-26 21:43:44 +0900

    N번째 큰 수
    Solved

---
+++ BOJ#2075/src/Main.java
@@ -0,0 +1,35 @@
+/**
+ * @author Minha Gwon
+ * @date 2021. 1. 26.
+ * N번째 큰 수
+ * https://www.acmicpc.net/problem/2075 
+ */
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+		
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < N; j++) {
+				int a = sc.nextInt();
+				pq.add(a);
+			}
+		}
+	
+		while(N != 1) {
+			N--;
+		}
+		
+		System.out.println(pq.poll());
+	}
+
+}
