aa3ec22e0d3ffa51b7d7ea498da1b9487697e61f
Minha Kwon <minha61@naver.com>
2021-02-09 23:27:57 +0900

    Create Main.java

---
+++ BOJ#14499/src/Main.java
@@ -0,0 +1,20 @@
+/**
+ * @author Minha Gwon
+ * @date 2021. 2. 9.
+ * 주사위 굴리기
+ * https://www.acmicpc.net/problem/14499
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		
+	}
+}
