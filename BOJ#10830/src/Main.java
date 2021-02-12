7bca3851eac967bae82da2f3d8b4423a07f6b2e5
Minha Kwon <minha61@naver.com>
2021-02-12 23:44:18 +0900

    Create Main.java

---
+++ BOJ#10830/src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author Minha Gwon
+ * @date 2021. 2. 12.
+ * 행렬 제곱 
+ * https://www.acmicpc.net/problem/10830
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
