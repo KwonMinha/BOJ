2d60b40b5be7f9cdd0cbd17dbb4532913a3dd7f0
Minha Kwon <minha61@naver.com>
2021-02-13 16:59:24 +0900

    Update Main.java

--- BOJ#10830/src/Main.java
+++ BOJ#10830/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * 
  * @author Minha Gwon
@@ -7,10 +9,37 @@
  */
 
 public class Main {
+	public static int[][] map;
+	public static int[][] multiMap;
+	public static int N;
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		N = sc.nextInt();
+		int B = sc.nextInt();
+		
+		map = new int[N][N];
+		multiMap = new int[N][N];
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < N; j++) {
+				map[i][j] = sc.nextInt();
+			}
+		}
 
+		
+	}
+	
+	public static void MatrixMultiplication() {
+		for(int i = 0; i < N; i++) {
+		
+			for(int j = 0; j < N; j++) {
+				// 1행 1열 
+				for(int k = 0; k < N; k++) {
+					map[i][j] += map[i][k] * map[j][k];
+				}
+			}
+		}
 	}
 
 }
