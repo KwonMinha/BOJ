5a9297a26f8aee43cab153c2aa3f9a373c6ac30f
Minha Kwon <minha61@naver.com>
2021-02-14 23:42:56 +0900

    Create Main.java

---
+++ BOJ#14500/src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * @author Minha Gwon
+ * @date 2021. 2. 14.
+ * 테트로미노
+ * https://www.acmicpc.net/problem/14500
+ */
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	}
+
+}
