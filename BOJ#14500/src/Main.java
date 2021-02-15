6613c2748f83693f1f9af4dab13d1a3756c79d5f
Minha Kwon <minha61@naver.com>
2021-02-16 01:04:41 +0900

    Update Main.java

--- BOJ#14500/src/Main.java
+++ BOJ#14500/src/Main.java
@@ -5,129 +5,89 @@
  * https://www.acmicpc.net/problem/14500
  */
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
 	static int N, M;
-	static int[][] map, tetroMap;
-	static ArrayList<Point> tetrominoList;
+	static int[][] map;
+	static boolean[][] visited;
 	static int answer = Integer.MIN_VALUE;
 
-	public static int[] dx = {-1, 0, 1, 0}; // 상 우 하 좌 시계방향 순서 
+	public static int[] dx = {-1, 0, 1, 0}; // 상 우 하 좌 시계 방향 순서 
 	public static int[] dy = {0, 1, 0, -1};
+
+	static int ex[][] = {{0, 0, 0, 1}, {0, 1, 2, 1}, {0, 0, 0, -1}, {0, -1, 0, 1}}; // ㅜ로 나올 수 있는 4가지 모양 미리 만듬 
+	static int ey[][] = {{0, 1, 2, 1}, {0, 0, 0, 1}, {0, 1, 2, 1}, {0, 1, 1, 1}};
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		M = sc.nextInt();
-		map = new int[N][M];
+		map = new int[N+1][M+1];
 
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < M; j++) {
+		for(int i = 1; i <= N; i++) {
+			for(int j = 1; j <= M; j++) {
 				map[i][j] = sc.nextInt();
 			}
 		}
 
-		tetroMap = new int[N][M];
+		for(int i = 1; i <= N; i++) {
+			for(int j = 1; j <= M; j++) {
+				visited = new boolean[N+1][M+1];
+				visited[i][j] = true;
+				dfs(1, i, j, map[i][j]);
+				visited[i][j] = false;
 
-		dfs(0);
-		
+				exShape(i, j);
+			}
+		}
+
 		System.out.println(answer);
 	}
 
-	// dfs로 4개의 정사각형을 NxM 배열에 모두 놓아본다. 
-	static void dfs(int depth) {
+	// DFS로 ㅜ 제외 4가지 모양 최대합 검사 
+	public static void dfs(int depth, int x, int y, int sum) {
 		if(depth == 4) { // 4개의 정사각형이 모두 놓아진 경우 
-			bfs();
-
+			answer = Math.max(answer, sum);
 			return;
 		}
 
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < M; j++) {
-				if(tetroMap[i][j] == 0) {
-					tetroMap[i][j] = 1;
-					dfs(depth+1);
-					tetroMap[i][j] = 0; // 놓은 이후, 다음 dfs를 위해 다시 원상태 0으로 돌려줌 
+		for(int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+
+			if(nx < 1 || nx > N || ny < 1 || ny > M) 
+				continue;
+
+			if(!visited[nx][ny]) {
+				visited[nx][ny] = true;
+				dfs(depth+1, nx, ny, sum + map[nx][ny]);
+				visited[nx][ny] = false;
+			}
+		}
+	}
+
+	// ㅜ 모양 검사 
+	public static void exShape(int x, int y){
+		for(int i = 0; i < 4; i++){
+			boolean isOut = false;
+			int sum = 0;
+
+			for(int j = 0; j < 4; j++){
+				int nx = x + ex[i][j];
+				int ny = y + ey[i][j];
+
+				if(nx < 0 || nx > N || ny < 0 || ny > M) {
+					isOut = true;
+					break;
+				}
+				else {
+					sum += map[nx][ny];
 				}
 			}
+
+			if(!isOut) 
+				answer = Math.max(answer, sum);
 		}
-	}
-
-	// bfs를 이용해 놓여진 정사각형이 테트로미노인지 확인 
-	static void bfs() {
-		tetrominoList = new ArrayList<>();
-		Queue<Point> queue = new LinkedList<>();
-		boolean[][] visited = new boolean[N][M];
-
-		loop:
-			for(int i = 0; i < N; i++) {
-				for(int j = 0; j < M; j++) {
-					if(tetroMap[i][j] == 1) {
-						queue.add(new Point(i, j)); // 정사각형이 놓여진 첫번째 위치를 찾고, 그 위치부터 bfs 시작 
-						tetrominoList.add(new Point(i, j));
-						visited[i][j] = true;
-
-						break loop;
-					}
-				}
-			}
-
-		while(!queue.isEmpty()) {
-			int cx = queue.peek().x;
-			int cy = queue.poll().y;
-
-			for(int i = 0; i < 4; i++) {
-				int nx = cx + dx[i];
-				int ny = cy + dy[i];
-
-				if(nx < 0 || nx >= N || ny < 0 || ny >= M) 
-					continue;
-
-				if(!visited[nx][ny] && tetroMap[nx][ny] == 1) {
-					queue.add(new Point(nx, ny));
-					tetrominoList.add(new Point(nx, ny));
-					visited[nx][ny] = true;
-				}
-			}
-		}
-
-		if(tetrominoList.size() == 4) { // 4개의 정사각형이 연결되어 있는 경우 -> 테트로미노 
-			tetromino();
-		}
-	}
-
-	// 테트로미노가 놓인 칸에 쓰여 있는 수들의 최대합 구하기 
-	static void tetromino() {
-		int sum = 0;
-		for(Point p : tetrominoList) {
-			sum += map[p.x][p.y];
-		}
-		answer = Math.max(answer, sum);
-	}
-
-	static void print(int[][] arr) {
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < M; j++) {
-				System.out.print(arr[i][j] + " ");
-			}
-			System.out.println();
-		}
-
-		System.out.println();
-	}
-
-}
-
-class Point {
-	int x;
-	int y;
-
-	Point(int x, int y) {
-		this.x = x;
-		this.y = y;
 	}
 }
