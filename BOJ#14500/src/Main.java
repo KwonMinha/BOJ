eb2d5cced7580949d1992065bfc8351911185c7b
Minha Kwon <minha61@naver.com>
2021-02-16 01:19:43 +0900

    테트로미노
    Solved

--- BOJ#14500/src/Main.java
+++ BOJ#14500/src/Main.java
@@ -16,24 +16,26 @@ public class Main {
 	public static int[] dx = {-1, 0, 1, 0}; // 상 우 하 좌 시계 방향 순서 
 	public static int[] dy = {0, 1, 0, -1};
 
-	static int ex[][] = {{0, 0, 0, 1}, {0, 1, 2, 1}, {0, 0, 0, -1}, {0, -1, 0, 1}}; // ㅜ로 나올 수 있는 4가지 모양 미리 만듬 
+	// (0, 0) 기준 - ㅜ로 나올 수 있는 4가지 모양 미리 만듬 
+	static int ex[][] = {{0, 0, 0, 1}, {0, 1, 2, 1}, {0, 0, 0, -1}, {0, -1, 0, 1}}; 
 	static int ey[][] = {{0, 1, 2, 1}, {0, 0, 0, 1}, {0, 1, 2, 1}, {0, 1, 1, 1}};
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		M = sc.nextInt();
-		map = new int[N+1][M+1];
+		map = new int[N+1][M+1]; // (0, 0) 기준 ㅜ 모양이 한칸씩 범위 밖으로 나가는 것을 고려해 map크기 +1 
 
 		for(int i = 1; i <= N; i++) {
 			for(int j = 1; j <= M; j++) {
 				map[i][j] = sc.nextInt();
 			}
 		}
-
+		
+		visited = new boolean[N+1][M+1];
+		
 		for(int i = 1; i <= N; i++) {
 			for(int j = 1; j <= M; j++) {
-				visited = new boolean[N+1][M+1];
 				visited[i][j] = true;
 				dfs(1, i, j, map[i][j]);
 				visited[i][j] = false;
@@ -47,7 +49,7 @@ public class Main {
 
 	// DFS로 ㅜ 제외 4가지 모양 최대합 검사 
 	public static void dfs(int depth, int x, int y, int sum) {
-		if(depth == 4) { // 4개의 정사각형이 모두 놓아진 경우 
+		if(depth == 4) { // 4개의 정사각형이 모두 놓아진 경우 종료 
 			answer = Math.max(answer, sum);
 			return;
 		}
@@ -56,7 +58,7 @@ public class Main {
 			int nx = x + dx[i];
 			int ny = y + dy[i];
 
-			if(nx < 1 || nx > N || ny < 1 || ny > M) 
+			if(nx < 1 || nx > N || ny < 1 || ny > M) // 범위 넘어가면 pass 
 				continue;
 
 			if(!visited[nx][ny]) {
@@ -73,7 +75,7 @@ public class Main {
 			boolean isOut = false;
 			int sum = 0;
 
-			for(int j = 0; j < 4; j++){
+			for(int j = 0; j < 4; j++) {
 				int nx = x + ex[i][j];
 				int ny = y + ey[i][j];
 
