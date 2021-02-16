0154eb4e3cdcd9470d247798ff74703c7fdcc018
Minha Kwon <minha61@naver.com>
2021-02-16 22:31:11 +0900

    Create Main.java

---
+++ BOJ#6549/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * @author Minha Gwon
+ * @date 2021. 2. 16.
+ * 히스토그램에서 가장 큰 직사각형 
+ * https://www.acmicpc.net/problem/6549
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
