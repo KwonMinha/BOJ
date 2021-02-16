1da30d4115d4230ab0e07199cb1b36bcabd6ab47
Minha Kwon <minha61@naver.com>
2021-02-16 23:29:10 +0900

    Create Main.java

---
+++ BOJ#2225/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * @author Minha Gwon
+ * @date 2021. 2. 16.
+ * 합분해 
+ * https://www.acmicpc.net/problem/2225
+ */
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+	}
+
+}
