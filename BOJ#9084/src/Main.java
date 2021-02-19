7ef3c667a7417eb46cf41e5ec9b8112f60f75150
Minha Kwon <minha61@naver.com>
2021-02-20 05:33:52 +0900

    Create Main.java

---
+++ BOJ#9084/src/Main.java
@@ -0,0 +1,27 @@
+/**
+ * @author Minha Gwon
+ * @date 2021. 2. 20.
+ * 동전 
+ * https://www.acmicpc.net/problem/9084
+ */
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for(int t = 0; t < T; t++) {
+			int N = sc.nextInt();
+			int[] coins = new int[N];
+			for(int i = 0; i < N; i++) {
+				coins[i] = sc.nextInt();
+			}
+			int M = sc.nextInt();
+		}
+	}
+
+
+}
