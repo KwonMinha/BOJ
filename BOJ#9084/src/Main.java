0daf8889de23fc3e0aa31b0cb431ed4f3515a493
Minha Kwon <minha61@naver.com>
2021-02-20 20:51:50 +0900

    동전
    Solved

--- BOJ#9084/src/Main.java
+++ BOJ#9084/src/Main.java
@@ -12,26 +12,28 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
-		
+
 		for(int t = 0; t < T; t++) {
 			int N = sc.nextInt(); // 동전의 가지 수
-			
-			int[] coins = new int[N];
-			for(int i = 0; i < N; i++) { // 동전의 각 금액 - 오름차순 정렬되어 있음 
-				coins[i] = sc.nextInt();
+
+			int[] coins = new int[N+1]; // dp가 1원인 1번 인덱스부터 시작하기 때문에 coins배열도 1부터 
+			for(int i = 1; i < N+1; i++) { 
+				coins[i] = sc.nextInt(); // 동전의 각 금액 - 오름차순 정렬
 			}
-			
+
 			int M = sc.nextInt(); // N가지 동전으로 만들어야 할 금액
-			
-			// 가장 많은 코인을 사용해서 M을 만드는 경우 -> 가장 값이 작은 동전만을 사용해서 만듬 
-			// -> 이 값이 dp 배열의 열의 개수 
-			int maxCnt = coins[0] % M == 0 ? coins[0] / M : coins[0] / M + 1;
-			
-			int[][] dp = new int[coins.length][maxCnt+1]; // 0개 쓰는 경우도 고려해야하기 때문에 maxCnt+1
-			
-			
-			
-			
+
+			int[] dp = new int[M+1]; // 계산 편의를 위해 1원부터 시작하는걸로 치고 +1
+
+			dp[0] = 1; // dp 초기값 
+
+			for (int i = 1; i < N+1; i++) {
+				for (int j = coins[i]; j <= M; j++) {
+					dp[j] += dp[j-coins[i]];
+				}
+			}
+
+			System.out.println(dp[M]);
 		}
 	}
 
