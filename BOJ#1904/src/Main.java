5d67f74fdbe0ffe3571b71e664409e1e82270de2
Minha Kwon <minha61@naver.com>
2021-02-24 01:45:28 +0900

    01타일
    Solved

--- BOJ#1904/src/Main.java
+++ BOJ#1904/src/Main.java
@@ -12,6 +12,20 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
+
+		int[] dp = new int[N+1];
+		
+		dp[1] = 1;
+		
+		if(N > 1)
+			dp[2] = 2;
+		
+		for(int i = 3; i < N+1; i++) {
+			dp[i] = dp[i-1] + dp[i-2];
+			dp[i] %= 15746;
+		}
+		
+		System.out.println(dp[N]);
 	}
 
 }
