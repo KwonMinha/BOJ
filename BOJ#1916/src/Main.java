68ce926203d942c9b690960523bc0a078385528e
Minha Kwon <minha61@naver.com>
2021-03-06 23:32:12 +0900

    Create Main.java

---
+++ BOJ#1916/src/Main.java
@@ -0,0 +1,18 @@
+/**
+ * @author Minha Gwon
+ * @date 2021. 3. 6.
+ * 최소비용 구하기 
+ * https://www.acmicpc.net/problem/1916
+ */
+
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+	}
+
+}
