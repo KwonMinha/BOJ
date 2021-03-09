45e2ef10f4538598e6f5a2d33972bd63157f4b36
Minha Kwon <minha61@naver.com>
2021-03-09 23:29:27 +0900

    Update Main.java

--- BOJ#1916/src/Main.java
+++ BOJ#1916/src/Main.java
@@ -12,7 +12,21 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		
 		int N = sc.nextInt();
+		int M = sc.nextInt();
+		
+		for(int i = 0; i < M; i++) {
+			int start = sc.nextInt();
+			int end = sc.nextInt();
+			int cost = sc.nextInt();
+			
+			
+		}
+		
+		int departure = sc.nextInt();
+		int arrival = sc.nextInt();
+		
 	}
 
 }
