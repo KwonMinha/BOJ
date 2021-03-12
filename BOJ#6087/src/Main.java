43ece3a969ab1e6be04c00abc4d41da75c9d6bd3
Minha Kwon <minha61@naver.com>
2021-03-12 14:38:56 +0900

    Update Main.java

--- BOJ#6087/src/Main.java
+++ BOJ#6087/src/Main.java
@@ -20,6 +20,8 @@ public class Main {
 	static int min = Integer.MAX_VALUE;
 
 	static char[][] map;
+	
+	static int[][] visited;
 
 	static int[] dx = {-1, 0, 1, 0};
 	static int[] dy = {0, 1, 0, -1};
@@ -49,25 +51,28 @@ public class Main {
 				}
 			}
 		}
-		
+
 		bfs();
-		
+
 		System.out.println(min);
+	}
 
-		//		for(int i = 0; i < H; i++) {
-		//			for(int j = 0; j < W; j++) {
-		//				System.out.print(map[i][j] + " ");
-		//			}
-		//			System.out.println();
-		//		}
-
+	public static void print() {
+		System.out.println();
+		for(int i = 0; i < H; i++) {
+			for(int j = 0; j < W; j++) {
+				System.out.print(visited[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
 	}
 
 	public static void bfs() {
 		Queue<Point> queue = new LinkedList<>();
 		queue.add(startPoint);
 
-		int[][] visited = new int[H][W];
+		visited = new int[H][W];
 		visited[startPoint.x][startPoint.y] = 1;
 
 		while(!queue.isEmpty()) {
@@ -91,16 +96,24 @@ public class Main {
 
 				int nm = cm; // 새로운 거울 개수 구하기 (꺾이면 +1)
 
-				if(cd != -1 || cd != nd) // 처음 시작이 아니고, 뱡향이 다른 경우 -> 꺾임 -> 거울 설치해야 함
+				if(cd != -1 || cd != nd) { // 처음 시작이 아니고, 뱡향이 다른 경우 -> 꺾임 -> 거울 설치해야 함
 					nm += 1; // 꺽임 
+					System.out.println("방향 바뀜 nm : " + nm);
+				}
 
 				if(visited[nx][ny] == 0) { // 방문하지 않은 경우 
 					visited[nx][ny] = nm;
 					queue.add(new Point(nx, ny, nd, nm));
+					
+					System.out.println("방문 X ");
+					print();
 				} else if(visited[nx][ny] >= nm) { 
 					// 이미 방문한 경우, 거울 개수가 더 작은 값으로 갱신 
 					visited[nx][ny] = nm; 
 					queue.add(new Point(nx, ny, nd, nm));
+					
+					System.out.println("방문 0 ");
+					print();
 				}
 			}
 		}
