d46c7a63395a8383b66c1c2addfb5ff927652e84
Minha Kwon <minha61@naver.com>
2021-03-16 21:56:54 +0900

    Create Main.java

---
+++ BOJ#14461/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * @author Minha Gwon
+ * @date 2021. 3. 16.
+ * 소가 길을 건너간 이유 7
+ * https://www.acmicpc.net/problem/14461
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
