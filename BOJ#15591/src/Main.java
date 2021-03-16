d4f12a38adbb0f3ffe76009e618ae40fc2910191
Minha Kwon <minha61@naver.com>
2021-03-16 23:32:21 +0900

    Create Main.java

---
+++ BOJ#15591/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * @author Minha Gwon
+ * @date 2021. 3. 16.
+ * MooTube (Silver) 
+ * https://www.acmicpc.net/problem/14461
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
