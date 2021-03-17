c58989f9cd33a6ff1c77e1990430be18802a6a43
Minha Kwon <minha61@naver.com>
2021-03-18 05:16:23 +0900

    Update Main.java

--- BOJ#1753/src/Main.java
+++ BOJ#1753/src/Main.java
@@ -5,11 +5,66 @@
  * https://www.acmicpc.net/problem/1753
  */
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 
+		int V = sc.nextInt();
+		int E = sc.nextInt();
+
+		int start = sc.nextInt();
+
+		int[][] arr = new int[V+1][V+1];
+		for(int i = 0; i < E; i++) {
+			int u = sc.nextInt();
+			int v = sc.nextInt();
+			int w = sc.nextInt();
+
+			// 서로 다른 두 정점 사이에 여러 개의 간선이 존재할 수도 있음 -> 최소값으로 갱신 
+			if(arr[u][v] == 0) {
+				arr[u][v] = w;
+			} else if(arr[u][v] > w) {
+				arr[u][v] = w;
+			}
+		}
+
+		boolean[] check = new boolean[V+1];
+
+		int[] distance = new int[V+1];
+		int INF = 10 * (V-1) + 1; // INF는 항상 (간선 가중치의 최댓값) * (정점 개수 - 1) 보다 큰 값을 사용해야 한다.
+		Arrays.fill(distance, INF);
+		distance[start] = 0;
+
+		for(int i = 0; i < V-1; i++) {
+			int min = Integer.MAX_VALUE;
+			int index = -1;
+
+			for(int j = 1; j < V+1; j++) {
+				if(!check[j] && distance[j] < min) {
+					min = distance[j];
+					index = j;
+				}
+			}
+
+			check[index] = true;
+
+			for(int j = 1; j < V+1; j++) {
+				if(!check[j] && arr[index][j] != 0 && distance[index] + arr[index][j] < distance[j]) {
+					distance[j] = distance[index] + arr[index][j];
+				}
+			}
+		}
+
+		for(int i = 1; i < V+1; i++) {
+			if(distance[i] ==  INF)
+				System.out.println("INF");
+			else
+				System.out.println(distance[i]);
+		}
 	}
 
 }
