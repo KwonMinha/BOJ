eb10f73b8865e1bbd04bd5774d1ca90b71698b6e
Minha Kwon <minha61@naver.com>
2021-03-30 21:09:36 +0900

    문자열 폭발
    Solved

--- BOJ#9935/src/Main.java
+++ BOJ#9935/src/Main.java
@@ -5,11 +5,50 @@
  * https://www.acmicpc.net/problem/9935
  */
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 
+		String str = sc.nextLine();
+		String frula = sc.nextLine();
+
+		StringBuilder result = new StringBuilder();
+
+		Stack<Character> stack = new Stack<>();
+		
+		for(int i = str.length()-1; i >= 0; i--) {
+			stack.push(str.charAt(i));
+
+			if(stack.size() >= frula.length() && stack.peek() == frula.charAt(0)) {
+				boolean isBomb = true;
+				for(int j = 1; j < frula.length(); j++) {
+					if(stack.get(stack.size()-j-1) != frula.charAt(j)) {
+						isBomb = false;
+						break;
+					}
+				}
+
+				if(isBomb) {
+					for(int j = 0; j < frula.length(); j++) {
+						stack.pop();
+					}
+				}
+			}
+		}
+		
+		if(stack.isEmpty()) {
+			System.out.println("FRULA");
+		} 
+		
+		while(!stack.isEmpty()) {
+			result.append(stack.pop());
+		}
+
+		System.out.println(result);
 	}
 
 }
