c1567369f1c4dac750cb0d9cc6646663b450a909
Minha Kwon <minha61@naver.com>
2021-03-31 20:59:48 +0900

    N-Queen
    Solved

---
+++ BOJ#9663/src/Main.java
@@ -0,0 +1,55 @@
+/**
+ * @author Minha Gwon
+ * @date 2021. 3. 31.
+ * N-Queen
+ * https://www.acmicpc.net/problem/9663
+ */
+
+import java.util.Scanner;
+
+public class Main {
+	static int N;
+	static int[] arr;
+	static int count = 0;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		N = sc.nextInt();
+
+		arr = new int[N];
+
+		nQueen(0);
+		
+		System.out.println(count);
+	}
+
+	public static void nQueen(int depth) {
+		// 모든 원소를 다 채운 상태면 count 증가 및 return 
+		if (depth == N) {
+			count++;
+			return;
+		}
+
+		for (int i = 0; i < N; i++) {
+			arr[depth] = i;
+			
+			// 놓을 수 있는 위치일 경우 재귀호출
+			if (Possibility(depth)) {
+				nQueen(depth + 1);
+			}
+		}
+	}
+
+	public static boolean Possibility(int col) {
+		for (int i = 0; i < col; i++) {
+			if (arr[col] == arr[i]) { // 같은 행에 존재할 경우 (해당 열의 행과 i열의 행이 일치할 경우) 
+				return false;
+			} else if (Math.abs(col - i) == Math.abs(arr[col] - arr[i])) { // 대각선상에 놓여있는 경우 (열의 차와 행의 차가 같을 경우)
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
