d62d0f0176d8c0f3869f8d3aaede587c02af287d
Minha Kwon <minha61@naver.com>
2021-04-01 01:23:22 +0900

    히스토그램에서 가장 큰 직사각형
    Solved

--- BOJ#6549/src/Main.java
+++ BOJ#6549/src/Main.java
@@ -1,32 +1,67 @@
 /**
  * @author Minha Gwon
- * @date 2021. 2. 16.
+ * @date 2021. 4. 1.
  * 히스토그램에서 가장 큰 직사각형 
  * https://www.acmicpc.net/problem/6549
  */
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
 		
 		while(true) {
-			int N = sc.nextInt();
+			st = new StringTokenizer(br.readLine());
 			
-			if(N == 0)
-				break;
+			int n = Integer.parseInt(st.nextToken());
 			
-			int[] histogram = new int[N];
-			for(int i = 0; i < N; i++) {
-				histogram[i] = sc.nextInt();
-				System.out.print(histogram[i] + " ");
+			if(n == 0) break;
+			
+			long[] arr = new long[n+2];
+			for(int i = 1; i < n+1; i++) {
+				arr[i] = Long.parseLong(st.nextToken());
 			}
-			System.out.println();
 			
+			long ans = 0;
+			
+			Stack<Integer> stack = new Stack<>();
+			
+			stack.push(0);
+			
+			for (int i = 1; i <= n+1; i++) {
+			    while(!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
+			        long height = arr[stack.peek()];
+			        
+			        stack.pop();
+		
+			        int width = i - stack.peek() - 1;
+
+			        ans = Math.max(ans, width * height);
+			    }
+			    
+			    stack.push(i);
+			}
+			
+			sb.append(ans + "\n");
 		}
 		
+		bw.write(sb.toString());
+		
+		bw.flush();
+		bw.close();
+		br.close();
 	}
+	
 
 }
