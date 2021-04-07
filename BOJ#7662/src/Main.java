c5cf699016d2aa410d0a59f048e0fb300268137e
Minha Kwon <minha61@naver.com>
2021-04-07 17:27:57 +0900

    이중 우선순위 큐
    Solved

--- BOJ#7662/src/Main.java
+++ BOJ#7662/src/Main.java
@@ -1,6 +1,6 @@
 /**
  * @author Minha Gwon
- * @date 2021. 4. 5.
+ * @date 2021. 4. 7.
  * 이중 우선순위 큐 
  * https://www.acmicpc.net/problem/7662
  */
@@ -12,10 +12,9 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
+import java.util.TreeMap;
 
 public class Main {
-	static int lastNum = 0;
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -26,34 +25,44 @@ public class Main {
 
 		for(int t = 0; t < T; t++) {
 			int K = Integer.parseInt(br.readLine());
-
-			PriorityQueue<Integer> pq = new PriorityQueue<>();
+			
+			TreeMap<Integer, Integer> treeMap = new TreeMap<>();
 
 			for(int k = 0; k < K; k++) {
 				st = new StringTokenizer(br.readLine());
 				String oper = st.nextToken();
 				int n = Integer.parseInt(st.nextToken());	
 
-				if(oper.equals("I")) {
-					pq.add(n);
-				} else if(oper.equals("D") && !pq.isEmpty()){
-					if(n == -1) {
-						pq.poll();
-					} else {
-						pq = removeLast(pq);
+				if(oper.equals("I")) { // 삽입 
+					treeMap.put(n, treeMap.getOrDefault(n, 0) + 1);
+				} else if(oper.equals("D") && !treeMap.isEmpty()){
+					if(n == -1) { // 최솟값 삭제 
+						int firstKey = treeMap.firstKey();
+						int firstValue = treeMap.get(firstKey);
+						
+						if(firstValue == 1) { 
+							treeMap.pollFirstEntry(); // 중복값이 없는 경우만 완전히 삭제 
+						} else {
+							treeMap.put(firstKey, firstValue - 1); // 중복일 경우 -1 
+						}
+					} else { // 최댓값 삭제 
+						int lastKey = treeMap.lastKey();
+						int lastValue = treeMap.get(lastKey);
+						
+						if(lastValue == 1) { 
+							treeMap.pollLastEntry(); // 중복값이 없는 경우만 완전히 삭제 
+						} else {
+							treeMap.put(lastKey, lastValue - 1); // 중복일 경우 -1 
+						}
 					}
 				}
 			}
 
 			// 정답 출력 
-			if(pq.isEmpty()) {
+			if(treeMap.isEmpty()) {
 				sb.append("EMPTY\n");
-			} else if(pq.size() == 1) {
-				int num = pq.poll();
-				sb.append(num + " " + num + "\n");
 			} else {
-				pq = removeLast(pq);
-				sb.append(lastNum + " " + pq.poll() + "\n");
+				sb.append(treeMap.lastKey() + " " + treeMap.firstKey() + "\n");
 			}
 		}
 		
@@ -62,20 +71,6 @@ public class Main {
 		bw.flush();
 		bw.close();
 		br.close();
-	}
-
-	public static PriorityQueue<Integer> removeLast(PriorityQueue<Integer> pq) {
-		PriorityQueue<Integer> pqNew = new PriorityQueue<>();
-
-		while(pq.size() > 1) {
-			pqNew.add(pq.poll());
-		}
-		
-		lastNum = pq.poll();
-
-		pq.clear();
-		
-		return pqNew;
 	}
 
 }
