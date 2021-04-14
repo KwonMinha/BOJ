c0c61692804aca48626e6b5f8d69693d8650d642
Minha Kwon <minha61@naver.com>
2021-04-14 19:53:52 +0900

    Update Main.java

--- BOJ#14890/src/Main.java
+++ BOJ#14890/src/Main.java
@@ -30,20 +30,36 @@ public class Main {
 			}
 		}
 
+		int ans = 0;
+
 		for(int i = 0; i < N; i++) { 
+			int[] col = new int[N];
+			int[] row = new int[N];
+			
 			for(int j = 0; j < N; j++) {
-				int[] col = new int[N];
-				int[] row = new int[N];
-
-				// i, j 행 검사 
 				col[j] = map[i][j];
-				solve(col);
-
-				// j, i 열 검사 
 				row[j] = map[j][i];
 			}
-
+			
+			// i, j 행 검사 
+			System.out.println("\n----col " + i);
+			if(solve(col)) { // 길을 지나갈 수 있다면 +1
+				ans++; 
+				System.out.println("*********succ");
+			}
+			
+			// j, i 열 검사 
+			System.out.println("\n----row " + i);
+			if(solve(row)) {// 길을 지나갈 수 있다면 +1
+				ans++; 
+				System.out.println("************succ");
+			}
 		}
+		
+		System.out.println();
+
+
+		System.out.println(ans);
 
 	}
 
@@ -51,9 +67,11 @@ public class Main {
 		boolean flag = true;
 
 		// 앞부터 검사 
+		System.out.println("front");
 		int preHeight = arr[0];
 		int sameHeightCnt = 1; // 0번째 칸은 넣고 시작하니 1부터 시작 
-		for(int i = 1; i < N-1; i++) {
+
+		for(int i = 1; i < N; i++) {
 			if(preHeight > arr[i]) { // 이전 높이보다 작으면 경사로 놓을 수 X
 				flag = false;
 				break;
@@ -62,7 +80,7 @@ public class Main {
 				continue;
 			} else if(arr[i] - preHeight == 1) { // 이전 높이보다 현재 높이가 1 큰 경우 (높이차 1) -> 경사로 놓을 수 있는지 검증 
 				if(sameHeightCnt >= L) { // L개의 높이가 같은 연속된 칸이 있는 경우 -> 경사로 놓을 수 O 
-					sameHeightCnt = 0; // 경사로 세웠으니 cnt 초기화 
+					sameHeightCnt = 1; // 경사로 세웠으니 cnt 초기화 
 					preHeight = arr[i];
 				} else {
 					flag = false;
@@ -76,16 +94,44 @@ public class Main {
 
 		if(flag) return true; // 앞부터 검사한 길을 지나갈 수 있다면 성공 
 
-		flag = true;
+		System.out.println("back");
 		// 앞부터 검사한게 실패라면 -> 뒤부터 다시 검사 
+		flag = true;
 		preHeight = arr[N-1];
 		sameHeightCnt = 1;
-		for(int i = N-2; i > 0; i--) {
 
+		for(int i = N-2; i >= 0; i--) {
+			System.out.println("pre : " + preHeight + ", cur i : " + arr[i]);
+			if(preHeight > arr[i]) { // 이전 높이보다 작으면 경사로 놓을 수 X
+				System.out.println("높이 작아!!");
+				flag = false;
+				break;
+			} else if (preHeight == arr[i]){ // 같으면 다음으로 넘어감 
+				System.out.println("높이 같아 넘어가 ");
+				sameHeightCnt++;
+				continue;
+			} else if(arr[i] - preHeight == 1) { // 이전 높이보다 현재 높이가 1 큰 경우 (높이차 1) -> 경사로 놓을 수 있는지 검증 
+				System.out.println("same : " + sameHeightCnt);
+				if(sameHeightCnt >= L) { // L개의 높이가 같은 연속된 칸이 있는 경우 -> 경사로 놓을 수 O 
+					System.out.println("높이차1 : 연속된거 있음 성공 ");
+					sameHeightCnt = 1; // 경사로 세웠으니 cnt 초기화 
+					preHeight = arr[i];
+				} else {
+					System.out.println("높이차1 : 연속 안됨 실패 ");
+					flag = false;
+					break; // L개 칸 연속 X, 경사로 놓을 수 X 
+				}
+			} else { // 높이차가 1보다 큰 경우 경사로 놓을수 X
+				System.out.println("높이차가 1보다 큼 요 ");
+				flag = false;
+				break;
+			}
 		}
 
-		if(flag) return true;
+		if(flag) return true; // 뒤부터 검사한 길을 지나갈 수 있다면 성공 
 
-		return false;
+
+		return false; // 실패 
 	}
+
 }
