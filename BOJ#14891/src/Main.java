4cda77d4a4211ad7557a1c5103c0a2845c03a2da
Minha Kwon <minha61@naver.com>
2021-04-17 02:00:30 +0900

    톱니바퀴
    Solved

--- BOJ#14891/src/Main.java
+++ BOJ#14891/src/Main.java
@@ -8,7 +8,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
@@ -19,6 +18,7 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
+		// 초기 톱니바퀴 구성해서 리스트에 저장 
 		for(int i = 0; i < 4; i++) {
 			int[] arr = Arrays.stream(br.readLine().split(""))
 					.mapToInt(Integer::parseInt)
@@ -27,23 +27,14 @@ public class Main {
 			wheelList[i] = new Wheel(arr);
 		}
 
+		// K번 회전 
 		int K = Integer.parseInt(br.readLine());
-
 		for(int i = 0; i < K; i++) {
 			st = new StringTokenizer(br.readLine());
 			int num = Integer.parseInt(st.nextToken()) -1;
 			int dir = Integer.parseInt(st.nextToken());
 
 			solve(num, dir);
-
-			System.out.println("done");
-			for(int k = 0; k < 4; k++) {
-				for(int l = 0; l < 8; l++) {
-					System.out.print(wheelList[k].arr[l] + " ");
-				}
-				System.out.println();
-			}
-			System.out.println();
 		}
 
 		// 점수 출력 
@@ -59,67 +50,51 @@ public class Main {
 	}
 
 	public static void solve(int num, int dir) {
-		int[] rotateOrder = new int[4];
+		int[] rotateOrder = new int[4]; // 맞닿는 왼쪽, 오른쪽 톱니바퀴 검사 후 회전해야할 방향 저장 
 		rotateOrder[num] = dir;
 
+		// 회전시킬 첫번째 톱니바퀴 
 		int[] startWheel = wheelList[num].arr;
 		int startLeft = startWheel[6];
 		int startRight = startWheel[2];
 		int startDir = dir;
 
-		// Up
-		System.out.println("up");
+		// Up - start 톱니바퀴를 기준으로 오른쪽의 톱니바퀴 검사 (start의 3시 방향 vs next의 9시 방향)
 		for(int i = num + 1; i < 4; i++) {
-			System.out.println("i : " + i);
-		
 			int[] nextWheel = wheelList[i].arr;
 			int nextLeft = nextWheel[6];
 			int nextRight = nextWheel[2];
 			
-			System.out.println("start Left : " + startLeft + ", startRight : " + startRight + ", nextL : " + nextLeft + ", nextRi : " + nextRight);
-			
-
-			if(startRight != nextLeft) {
-				System.out.println("OK");
+			if(startRight != nextLeft) { // N vs S극인 경우 
 				rotateOrder[i] = startDir == 1 ? -1 : 1;
 				startDir = rotateOrder[i];
 				startRight = nextRight;
 			} else {
-				System.out.println("X");
 				break;
 			}
 		}
 
-		// Down
-		System.out.println("down");
+		// Down - start 톱니바퀴를 기준으로 왼쪽의 톱니바퀴 검사 (next의 3시 방향 vs start의 9시 방향)
 		startLeft = startWheel[6];
 		startRight = startWheel[2];
 		startDir = dir;
 
 		for(int i = num - 1; i >= 0; i--) {
-			System.out.println("i : " + i);
-			
 			int[] nextWheel = wheelList[i].arr;
 			int nextRight = nextWheel[2];
 			int nextLeft = nextWheel[6];
 			
-			System.out.println("start Left : " + startLeft + ", startRight : " + startRight + ", nextL : " + nextLeft + ", nextRi : " + nextRight);
-			
-
-			if(startLeft != nextRight) {
-				System.out.println("OK");
+			if(startLeft != nextRight) { // N vs S극인 경우 
 				rotateOrder[i] = startDir == 1 ? -1 : 1;
 				startDir = rotateOrder[i];
 				startLeft = nextLeft;
 			} else {
-				System.out.println("X");
 				break;
 			}
 		}
 
 		// rotate
 		for(int i = 0; i < 4; i++) {
-			System.out.println("rotate : i : " + i  + ", " + rotateOrder[i]);
 			if(rotateOrder[i] == 1)
 				rotateRight(wheelList[i]);
 			else if(rotateOrder[i] == -1)
