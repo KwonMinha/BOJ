4c05374de75a9830ca31e25625224d98adc65211
Minha Kwon <minha61@naver.com>
2021-04-16 19:09:23 +0900

    경사로
    Solved

--- BOJ#14890/src/Main.java
+++ BOJ#14890/src/Main.java
@@ -21,8 +21,9 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		L = Integer.parseInt(st.nextToken());
 
-		map = new int[N][N];
+		int[][] map = new int[N][N];
 
+		// 초기 지도 구성 
 		for(int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j = 0; j < N; j++) {
@@ -31,107 +32,53 @@ public class Main {
 		}
 
 		int ans = 0;
-
-		for(int i = 0; i < N; i++) { 
-			int[] col = new int[N];
-			int[] row = new int[N];
-			
+		
+		// 행길, 열길로 나누어 지나갈 수 있는지 검사 
+		int[] col = new int[N];
+		int[] row = new int[N];
+		for(int i = 0; i < N; i++) {
 			for(int j = 0; j < N; j++) {
 				col[j] = map[i][j];
 				row[j] = map[j][i];
 			}
 			
-			// i, j 행 검사 
-			System.out.println("\n----col " + i);
-			if(solve(col)) { // 길을 지나갈 수 있다면 +1
-				ans++; 
-				System.out.println("*********succ");
-			}
+			// 지나갈 수 있다면 +1 
+			if(solve(col)) ans++; // 행 길 검사 
 			
-			// j, i 열 검사 
-			System.out.println("\n----row " + i);
-			if(solve(row)) {// 길을 지나갈 수 있다면 +1
-				ans++; 
-				System.out.println("************succ");
+			if(solve(row)) ans++; // 열 길 검사 
+		}
+
+		System.out.println(ans);
+	}
+	
+	public static boolean solve(int[] arr) {
+		boolean[] visited = new boolean[N]; // 경사로를 놓았는지 안 놓았는지 판별하기 위한 배열 
+		
+		for(int i = 1; i < N; i++) {
+			if(arr[i] == arr[i-1]) { // 높이가 같으면 pass 
+				continue;
+			} else if(arr[i] - arr[i-1] == 1) { // 오름차순으로 높이차 1인 경우  
+				for(int j = i-1; j >= i-L; j--) { // 현재 i칸 이전의 칸들 L만큼 검사 
+					// 범위를 벗어나고, 높이가 다르고, 경사로가 있는 곳이라면 실패 
+					if(j < 0 || arr[j] != arr[i]-1 || visited[j]) {
+						return false;
+					} 
+					visited[j] = true;
+				}
+			} else if(arr[i] - arr[i-1] == -1) { // 내림차순으로 높이차 1인 경우 
+				for(int j = i; j < i+L; j++) { // 현재 i칸 포함 이후의 칸들 L만큼 검사 
+					// 범위를 벗어나고, 높이가 다르고, 경사로가 있는 곳이라면 실패 
+					if(j >= N || arr[j] != arr[i] || visited[j]) {
+						return false;
+					} 
+					visited[j] = true;
+				}
+			} else { // 1보다 큰 높이차라면 실패 
+				return false;
 			}
 		}
 		
-		System.out.println();
-
-
-		System.out.println(ans);
-
-	}
-
-	public static boolean solve(int[] arr) {
-		boolean flag = true;
-
-		// 앞부터 검사 
-		System.out.println("front");
-		int preHeight = arr[0];
-		int sameHeightCnt = 1; // 0번째 칸은 넣고 시작하니 1부터 시작 
-
-		for(int i = 1; i < N; i++) {
-			if(preHeight > arr[i]) { // 이전 높이보다 작으면 경사로 놓을 수 X
-				flag = false;
-				break;
-			} else if (preHeight == arr[i]){ // 같으면 다음으로 넘어감 
-				sameHeightCnt++;
-				continue;
-			} else if(arr[i] - preHeight == 1) { // 이전 높이보다 현재 높이가 1 큰 경우 (높이차 1) -> 경사로 놓을 수 있는지 검증 
-				if(sameHeightCnt >= L) { // L개의 높이가 같은 연속된 칸이 있는 경우 -> 경사로 놓을 수 O 
-					sameHeightCnt = 1; // 경사로 세웠으니 cnt 초기화 
-					preHeight = arr[i];
-				} else {
-					flag = false;
-					break; // L개 칸 연속 X, 경사로 놓을 수 X 
-				}
-			} else { // 높이차가 1보다 큰 경우 경사로 놓을수 X
-				flag = false;
-				break;
-			}
-		}
-
-		if(flag) return true; // 앞부터 검사한 길을 지나갈 수 있다면 성공 
-
-		System.out.println("back");
-		// 앞부터 검사한게 실패라면 -> 뒤부터 다시 검사 
-		flag = true;
-		preHeight = arr[N-1];
-		sameHeightCnt = 1;
-
-		for(int i = N-2; i >= 0; i--) {
-			System.out.println("pre : " + preHeight + ", cur i : " + arr[i]);
-			if(preHeight > arr[i]) { // 이전 높이보다 작으면 경사로 놓을 수 X
-				System.out.println("높이 작아!!");
-				flag = false;
-				break;
-			} else if (preHeight == arr[i]){ // 같으면 다음으로 넘어감 
-				System.out.println("높이 같아 넘어가 ");
-				sameHeightCnt++;
-				continue;
-			} else if(arr[i] - preHeight == 1) { // 이전 높이보다 현재 높이가 1 큰 경우 (높이차 1) -> 경사로 놓을 수 있는지 검증 
-				System.out.println("same : " + sameHeightCnt);
-				if(sameHeightCnt >= L) { // L개의 높이가 같은 연속된 칸이 있는 경우 -> 경사로 놓을 수 O 
-					System.out.println("높이차1 : 연속된거 있음 성공 ");
-					sameHeightCnt = 1; // 경사로 세웠으니 cnt 초기화 
-					preHeight = arr[i];
-				} else {
-					System.out.println("높이차1 : 연속 안됨 실패 ");
-					flag = false;
-					break; // L개 칸 연속 X, 경사로 놓을 수 X 
-				}
-			} else { // 높이차가 1보다 큰 경우 경사로 놓을수 X
-				System.out.println("높이차가 1보다 큼 요 ");
-				flag = false;
-				break;
-			}
-		}
-
-		if(flag) return true; // 뒤부터 검사한 길을 지나갈 수 있다면 성공 
-
-
-		return false; // 실패 
+		return true;
 	}
 
 }
--- BOJ#14890/src/Main2.java
+++ BOJ#14890/src/Main2.java
@@ -14,8 +14,6 @@ public class Main2 {
 	static int[][] map;
 	static int N, L;
 
-	static int[][] colMap, rowMap;
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -25,6 +23,7 @@ public class Main2 {
 
 		int[][] map = new int[N][N];
 
+		// 초기 지도 구성 
 		for(int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j = 0; j < N; j++) {
@@ -34,6 +33,7 @@ public class Main2 {
 
 		int ans = 0;
 		
+		// 행길, 열길로 나누어 지나갈 수 있는지 검사 
 		int[] col = new int[N];
 		int[] row = new int[N];
 		for(int i = 0; i < N; i++) {
@@ -41,71 +41,56 @@ public class Main2 {
 				col[j] = map[i][j];
 				row[j] = map[j][i];
 			}
-
-			System.out.print("\ncol : ");
-			for(int j : col) {
-				System.out.print(j + " ");
-			}
+			
+			// 지나갈 수 있다면 +1 
 			if(solve(col)) ans++;
 			
-			System.out.print("\nrow : ");
-			for(int j : row) {
-				System.out.print(j + " ");
-			}
 			if(solve(row)) ans++;
-			
-			System.out.println();
 		}
 
 		System.out.println(ans);
 	}
 	
 	public static boolean solve(int[] arr) {
-		System.out.println();
-		boolean[] visited = new boolean[N];
+		boolean[] visited = new boolean[N]; // 경사로를 놓았는지 안 놓았는지 판별하기 위한 배열 
 		
 		for(int i = 1; i < N; i++) {
 			if(arr[i] == arr[i-1]) { // 높이가 같으면 pass 
-				System.out.println("same height");
 				continue;
-			} else if(arr[i] - arr[i-1] == 1) { // 오름차순 높이차 1 
-				System.out.println("height == 1");
-				int j = i-1;
+			} else if(arr[i] - arr[i-1] == 1) { // 오름차순으로 높이차 1인 경우  
+				int j = i-1; // 현재 i칸 이전의 칸들 검사 
 				int cnt = 0;
 				
-				while(j >= 0 && arr[j] == arr[i]-1 && cnt < L && !visited[j]) {
+				// 범위를 벗어나지 않고, 높이가 같으며, 경사로가 없는 곳이고, L칸이 연속되지 않을 때까지 검사 
+				while(j >= 0 && arr[j] == arr[i]-1 && !visited[j] && cnt < L ) {
 					cnt++;
 					j--;
 				}
-				System.out.println("cnt : " + cnt + ", j : " + j);
 				
-				if(cnt < L) {
-					System.out.println("fail L");
+				if(cnt < L) {  // L칸이 연속되지 않는 경우 실패 
 					return false;
 				} else {
-					System.out.println("succ L");
+					// L칸이 연속되는 경우, j인덱스를 거슬러 올라오며 경사로 놓음 
 					while(cnt != 0) {
 						visited[j+1] = true;
 						j++;
 						cnt--;
 					}
 				}
-			} else if(arr[i] - arr[i-1] == -1) { // 내림차순 높이차 1 
-				System.out.println("height == -1");
+			} else if(arr[i] - arr[i-1] == -1) { // 내림차순으로 높이차 1인 경우 
+				int j = i+1; // 현재 i칸 포함 이후의 칸들 검사 
 				int cnt = 1;
-				int j = i+1;
 				
-				while(j < N && arr[j] == arr[i] && cnt < L && !visited[j]) {
+				// 범위를 벗어나지 않고, 높이가 같으며, 경사로가 없는 곳이고, L칸이 연속되지 않을 때까지 검사 
+				while(j < N && arr[j] == arr[i] && !visited[j] && cnt < L) {
 					cnt++;
 					j++;
 				}
-				System.out.println("cnt : " + cnt + ", j : " + j);
 				
-				if(cnt < L) {
-					System.out.println("fail L");
+				if(cnt < L) { // L칸이 연속되지 않는 경우 실패 
 					return false;
 				} else {
-					System.out.println("succ L");
+					// L칸이 연속되는 경우, j인덱스를 거슬러 내려오며 경사로 놓음 
 					while(cnt != 0) {
 						visited[j-1] = true;
 						j--;
@@ -113,7 +98,6 @@ public class Main2 {
 					}
 				}
 			} else { // 1보다 큰 높이차라면 실패 
-				System.out.println("fail");
 				return false;
 			}
 		}
