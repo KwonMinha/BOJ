8daa007e5192170f52653bd55ad2b8ef061c8cf5
Minha Kwon <minha61@naver.com>
2021-04-16 19:12:21 +0900

    Create Main.java

---
+++ BOJ#14891/src/Main.java
@@ -0,0 +1,34 @@
+/**
+ * @author Minha Gwon
+ * @date 2021. 4. 16.
+ * 톱니바퀴 
+ * https://www.acmicpc.net/problem/14891
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[][] map;
+	static int N, L;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		L = Integer.parseInt(st.nextToken());
+
+		int[][] map = new int[N][N];
+
+		// 초기 지도 구성 
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+	}
+}
