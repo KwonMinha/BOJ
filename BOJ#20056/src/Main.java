322752f92c34a7b1a1536670f5751f251df3bf6e
Minha Kwon <minha61@naver.com>
2021-04-25 03:56:50 +0900

    Create Main.java

---
+++ BOJ#20056/src/Main.java
@@ -0,0 +1,24 @@
+/**
+ * @author Minha Gwon
+ * @date 2021. 4. 25.
+ * 마법사 상어와 파이어볼
+ * https://www.acmicpc.net/problem/20056
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int T = Integer.parseInt(st.nextToken());	
+		
+	}
+
+}
