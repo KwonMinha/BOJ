ff71c49cfb54003a52fd3ff8924ef84bfc5699fa
Minha Kwon <minha61@naver.com>
2021-04-28 22:19:51 +0900

    Create Main.java

---
+++ BOJ#2843/src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * @author Minha Gwon
+ * @date 2021. 4. 28.
+ * 마블 
+ * https://www.acmicpc.net/problem/2843
+ */
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args)  {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+	}
+
+}
