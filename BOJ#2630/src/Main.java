7e642645f55f35656b24cd93195f57b7f02e3ef5
Minha Kwon <minha61@naver.com>
2021-04-30 12:33:26 +0900

    색종이
    Solved

--- BOJ#2630/src/Main.java
+++ BOJ#2630/src/Main.java
@@ -11,85 +11,51 @@ import java.util.*;
 public class Main {
 	static int[][] arr;
 	static int N;
-	
-	static int white = 0;
+	static boolean[][] visited;
 	static int blue = 0;
+	static int white = 0;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		
+
 		N = Integer.parseInt(br.readLine());
-		
+
 		arr = new int[N][N];
-		
+		visited = new boolean[N][N];
+
 		for(int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			
+
 			for(int j = 0; j < N; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
-		if(check(N, 0, 0)) {
-			System.out.println("0\n1");
-		} else {
-			System.out.println("start");
-			solve(N/2, 0, 0);
-			
-			System.out.println(white + "\n" + blue);
-		}
+		solve(N, 0, 0);
+
+		System.out.println(white + "\n" + blue);
 	}
-	
+
 	public static void solve(int n, int x, int y) {
-		System.out.println("\n----n = " + n);
-		if(n == 1) {
-			System.out.println("n = 1");
-			if(arr[x][y] == 1) {
-				blue++;
-				return;
-			} else {
-				white++;
-				return;
-			}
-		}
-		
-		if(!check(n, x, y)) { // 1사분면 
-			System.out.println("1 X");
-			solve(n/2, x, y);
-		} else {
-			System.out.println("1 O - " + x + ", " + y);
+		if(visited[x][y]) {
 			return;
 		}
-		
-		if(!check(n, x, n/2*y)) { // 2사분면 
-			System.out.println("2 X");
-			solve(n/2, x, n/2*y);
-		} else {
-			System.out.println("2 O" + x + ", " + n/2*y);
+
+		if(check(n, x, y)) {
+			visited[x][y] = true;
 			return;
 		}
-		
-		if(!check(n, n/2*x, y)) { // 3사분면 
-			System.out.println("3 X");
-			solve(n/2, n/2*x, y);
-		} else {
-			System.out.println("3 O" + n/2*x + ", " + y);
-			return;
-		}
-		
-		if(!check(n, n/2*x, n/2*y)) { // 4사분면 
-			System.out.println("4 X");
-			solve(n/2, n/2*x, n/2*y);
-		} else {
-			System.out.println("4 O" + n/2*x + ", " + n/2*y);
-			return;
-		}
+
+		solve(n/2, x, y);
+		solve(n/2, x, n/2+y);
+		solve(n/2, n/2+x, y);
+		solve(n/2, n/2+x, n/2+y);
 	}
-	
+
 	public static boolean check(int n, int x, int y) {
 		int color = arr[x][y];
-		
+
 		for(int i = x; i < x+n; i++) {
 			for(int j = y; j < y+n; j++) {
 				if(arr[i][j] != color) {
@@ -98,14 +64,11 @@ public class Main {
 			}
 		}
 		
-		if(color == 1) {
-			System.out.println("blue++");
-			blue++;
-		} else {
-			System.out.println("white++");
+		if(color == 1) 
+			blue++; 
+		else 
 			white++;
-		}
-		
+
 		return true;
 	}
 
