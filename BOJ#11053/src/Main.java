555ab86856f0ab908d6c93c203de349c4ec101df
Minha Kwon <minha61@naver.com>
2021-05-05 23:49:33 +0900

    Create Main.java

---
+++ BOJ#11053/src/Main.java
@@ -0,0 +1,25 @@
+/**
+ * @author Minha Gwon
+ * @date 2021. 5. 5.
+ * 가장 긴 증가하는 부분 수열
+ * https://www.acmicpc.net/problem/11053
+ */
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args)  {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		int[] arr = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+	}
+
+}
