72d31ee874cd4db9167953258a82a165bcf39460
Minha Kwon <minha61@naver.com>
2021-05-06 01:19:06 +0900

    가장 긴 증가하는 부분 수열
    Solved

--- BOJ#11053/src/Main.java
+++ BOJ#11053/src/Main.java
@@ -5,7 +5,7 @@
  * https://www.acmicpc.net/problem/11053
  */
 
-import java.util.HashSet;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -16,10 +16,30 @@ public class Main {
 		int N = sc.nextInt();
 		
 		int[] arr = new int[N];
+		int[] dp = new int[N];
 		
 		for(int i = 0; i < N; i++) {
 			arr[i] = sc.nextInt();
 		}
+	
+		dp[0] = 1;
+		
+		for(int i = 1; i < N; i++) {
+			dp[i] = 1;
+			
+			for(int j = 0; j < i; j++) {
+				if(arr[j] < arr[i] && dp[i] < dp[j]+1) {
+					dp[i] = dp[j] + 1;
+				}
+			}
+		}
+		
+		int max = 0;
+		for(int i : dp) {
+			max = Math.max(max, i);
+		}
+		
+		System.out.println(max);
 	}
 
 }
