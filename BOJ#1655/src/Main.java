b89e4588ab640c44b41e4fd4cbac0c7e16b6c830
Minha Kwon <minha61@naver.com>
2021-06-29 23:42:30 +0900

    Create Main.java

---
+++ BOJ#1655/src/Main.java
@@ -0,0 +1,23 @@
+/**
+ * @author Minha Gwon
+ * @date 2021. 6. 29.
+ * 가운데를 말해요
+ * https://www.acmicpc.net/problem/1655
+ */
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		for(int i = 0; i < N; i++) {
+			
+		}
+
+	}
+
+}
