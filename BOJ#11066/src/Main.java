78ed30c6e5927edf62e070bfd8b3d1b0653de870
Minha Kwon <minha61@naver.com>
2021-07-06 00:09:11 +0900

    Create Main.java

---
+++ BOJ#11066/src/Main.java
@@ -0,0 +1,20 @@
+/**
+ * @author Minha Gwon
+ * @date 2021. 7. 5.
+ * 파일 합치기
+ * https://www.acmicpc.net/problem/11066
+ */
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+
+	}
+
+}
