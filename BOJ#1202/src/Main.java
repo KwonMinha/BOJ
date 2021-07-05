c0107fecc35b5e82e63fc690a9937da686e68b6e
Minha Kwon <minha61@naver.com>
2021-07-06 04:11:36 +0900

    보석 도둑
    Solved

--- BOJ#1202/src/Main.java
+++ BOJ#1202/src/Main.java
@@ -12,7 +12,6 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int N = Integer.parseInt(st.nextToken()); // 보석 개수 
