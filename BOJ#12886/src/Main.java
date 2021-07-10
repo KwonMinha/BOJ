a9eaec17a488cce5899f9f874ee002f531d5e187
Minha Kwon <minha61@naver.com>
2021-07-10 17:23:05 +0900

    Create Main.java

---
+++ BOJ#12886/src/Main.java
@@ -0,0 +1,20 @@
+/**
+ * @author Minha Gwon
+ * @date 2021. 7. 10.
+ * 돌그룹
+ * https://www.acmicpc.net/problem/12886
+ */
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+	}
+
+}
