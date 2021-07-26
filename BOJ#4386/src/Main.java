c19ea8e0e5df3b4efe0a4968f84150c433ec8b05
Minha Kwon <minha61@naver.com>
2021-07-27 02:45:52 +0900

    별자리 만들기
    Solved

---
+++ BOJ#4386/src/Main.java
@@ -0,0 +1,106 @@
+/**
+ * @author Minha Gwon
+ * @date 2021. 7. 27.
+ * 별자리 만들기 
+ * https://www.acmicpc.net/problem/4386
+ */
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+	static int N;
+	static ArrayList<Node> starList;
+	static int[] parent;
+	static double answer = 0;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		N = sc.nextInt();
+
+		Node[] star = new Node[N];
+		parent = new int[N];
+		starList = new ArrayList<>();
+
+		for(int i = 0; i < N; i++) {
+			double x = sc.nextDouble();
+			double y = sc.nextDouble();
+	
+			star[i] = new Node(x, y, 0);
+		}
+		
+		for(int i = 0; i < N; i++) {
+			parent[i] = i;
+		}
+
+		for(int i = 0; i < N-1; i++) {
+			Node n1 = star[i];
+
+			for(int j = i+1; j < N; j++) {
+				Node n2 = star[j];
+
+				double weight = Math.sqrt(Math.pow(n1.x - n2.x, 2) + Math.pow(n1.y - n2.y, 2));
+				starList.add(new Node(i, j, weight));
+			}
+		}
+		
+		Collections.sort(starList);
+		
+		for(int i = 0; i < starList.size(); i++) {
+			Node node = starList.get(i);
+			
+			int start = (int) node.x;
+			int end = (int) node.y;
+			
+			if(find(start) != find(end)) {
+				union(start, end);
+				answer += node.w;
+			}
+			
+//			int a = find(start);
+//			int b = find(end);
+//			
+//			if(a != b) {
+//				union(start, end);
+//				answer += node.w;
+//			}
+		}
+		
+		System.out.println(String.format("%.2f", answer));
+	}
+
+	static int find(int a) {
+		if(a == parent[a])
+			return a;
+
+		return parent[a] = find(parent[a]);
+	}
+
+	static void union(int a, int b) {
+		a = find(a);
+		b = find(b);
+
+		parent[b] = a;
+	}
+
+}
+
+class Node implements Comparable<Node> {
+	double x;
+	double y;
+	double w;
+
+	Node(double x, double y, double w) {
+		this.x = x;
+		this.y = y;
+		this.w = w;
+	}
+
+	@Override
+	public int compareTo(Node o) {
+		return o.w > this.w ? -1 : 1;
+	}
+
+}
